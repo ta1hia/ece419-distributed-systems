8357407f05fe05496dd3b7c35cec95f6a2a59c47
gumbak <kevin.gumba@gmail.com>
2014-01-13 13:45:01 -0500

    Create BrokerClient.java
    Adding.

---
+++ labs/lab1/broker/broker2/BrokerClient.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.net.*;
+
+public class BrokerClient {
+	public static void main(String[] args) throws IOException,
+			ClassNotFoundException {
+
+		Socket brokerSocket = null;
+		ObjectOutputStream out = null;
+		ObjectInputStream in = null;
+
+		try {
+			/* variables for hostname/port */
+			String hostname = "localhost";
+			int port = 4444;
+			
+			if(args.length == 2) {
+				hostname = args[0];
+				port = Integer.parseInt(args[1]);
+			} else {
+				System.err.println("ERROR: Invalid arguments!");
+				System.exit(-1);
+			}
+			brokerSocket = new Socket(hostname, port);
+
+			out = new ObjectOutputStream(brokerSocket.getOutputStream());
+			in = new ObjectInputStream(brokerSocket.getInputStream());
+
+		} catch (UnknownHostException e) {
+			System.err.println("ERROR: Don't know where to connect!!");
+			System.exit(1);
+		} catch (IOException e) {
+			System.err.println("ERROR: Couldn't get I/O for the connection.");
+			System.exit(1);
+		}
+
+		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+		String userInput;
+
+
+		System.out.print("Enter queries or quit for exit:");
+		while ((userInput = stdIn.readLine()) != null
+				&& userInput.toLowerCase().indexOf("x") == -1) {
+			
+			
+			/* re-print console prompt */
+			System.out.print("> ");
+	  	
+			/* make a new request packet */
+			BrokerPacket packetToServer = new BrokerPacket();
+			packetToServer.type = BrokerPacket.BROKER_REQUEST;
+			packetToServer.symbol = userInput.toLowerCase();
+			out.writeObject(packetToServer);
+
+			/* print server reply */
+			BrokerPacket packetFromServer;
+			packetFromServer = (BrokerPacket) in.readObject();
+
+			if (packetFromServer.type == BrokerPacket.BROKER_QUOTE)
+				System.out.println("Quote from broker: " + String.valueOf(packetFromServer.quote));
+            else {
+                /* error returned - this case isn't handled in Broker1 */
+                System.out.println("Quote from broker: 0");
+            }
+
+			/* re-print console prompt */
+			System.out.print("> ");
+		}
+
+		/* tell server that i'm quitting */
+		BrokerPacket packetToServer = new BrokerPacket();
+		packetToServer.type = BrokerPacket.BROKER_BYE;
+		//packetToServer.message = "Bye!";
+		out.writeObject(packetToServer);
+
+		out.close();
+		in.close();
+		stdIn.close();
+		brokerSocket.close();
+	}
+}
