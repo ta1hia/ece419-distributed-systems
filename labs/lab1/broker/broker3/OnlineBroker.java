d5a69bb47cd48205bf3e9a8dde7febc850cc6832
gumbak <kevin.gumba@gmail.com>
2014-01-18 23:02:36 -0500

    Update OnlineBroker.java
    Request lookup registration on startup.

--- labs/lab1/broker/broker3/OnlineBroker.java
+++ labs/lab1/broker/broker3/OnlineBroker.java
@@ -19,7 +19,7 @@ public class OnlineBroker {
 	// This is where server will be listening
         try { 
             if (args.length == 4) {
-                brokerSocket = new ServerSocket(Integer.parseInt(args[0]));
+                brokerSocket = new ServerSocket(Integer.parseInt(args[2]));
             } else {
                 System.err.println("Error: Invalid arguments!");
                 System.exit(-1);
@@ -31,9 +31,9 @@ public class OnlineBroker {
 
 	// Connect to the BrokerLookup thread.
 	try {
-	    /* variables for default brokerLookup thread */
-	    String hostname = "localhost";
-	    int port = 4444;
+	    /* Lookup variables. Do not hardwire! */
+	    String hostname = null;
+	    int port = 0;
 		
 	    if(args.length == 4) {
 		hostname = args[0];
@@ -46,23 +46,31 @@ public class OnlineBroker {
 	    out = new ObjectOutputStream(lookupSocket.getOutputStream());
 	    in = new ObjectInputStream(lookupSocket.getInputStream());
 
+	    // Register broker into BrokerLookup
+	    // $1 = hostname of lookup, $2 = port of BrokerLookupServer, $3 = port where I listen, $4 = key
+	    /* make a new request packet */
+	    BrokerPacket packetToLookup = new BrokerPacket();
+	    packetToLookup.type = BrokerPacket.LOOKUP_REGISTER;
+	    packetToLookup.symbol = args[3];
+	    packetToLookup.locations = new BrokerLocation[1];
+	    packetToLookup.locations[0] = new BrokerLocation(InetAddress.getLocalHost().getHostName(), Integer.parseInt(args[2]));
+	    
+	    //System.out.println("The stored locations are: " + packetToLookup.locations[0].broker_host);
+	    out.writeObject(packetToLookup);
+
 	} catch (UnknownHostException e) {
 	    System.err.println("ERROR: Don't know where to connect!!");
 	    System.exit(1);
 	} catch (IOException e) {
 	    System.err.println("ERROR: Couldn't get I/O for the connection.");
 	    System.exit(1);
+	} catch (NullPointerException e) {	
+	    System.err.println("ERROR: Null pointer exception.");
+	    System.exit(1);
 	}
 
-	// Register broker using BrokerLookup
-	// $1 = hostname, $2 = port of BrokerLookupServer, $3 = port where I listen, $4 = key
-	/* make a new request packet */
-	BrokerPacket packetToLookup = new BrokerPacket();
-	packetToLookup.type = BrokerPacket.LOOKUP_REQUEST;
-	packetToLookup.symbol = args[3];
-	packetToLookup.locations[0] = new BrokerLocation(InetAddress.getLocalHost().getHostName(), Integer.parseInt(args[2]));
-	out.writeObject(packetToLookup);
 
+	// Retrieve brokers from table
         /* Store table into a hashmap */
         ConcurrentHashMap<String, Long> table = new ConcurrentHashMap<String, Long>();
         BufferedReader input = new BufferedReader(new FileReader(args[3]));
