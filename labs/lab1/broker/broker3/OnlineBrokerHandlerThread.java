b3c0442d17ec1523b006cada43f22360a2d329ba
gumbak <kevin.gumba@gmail.com>
2014-01-20 00:34:19 -0500

    Update OnlineBrokerHandlerThread.java

--- labs/lab1/broker/broker3/OnlineBrokerHandlerThread.java
+++ labs/lab1/broker/broker3/OnlineBrokerHandlerThread.java
@@ -22,147 +22,231 @@ public class OnlineBrokerHandlerThread extends Thread {
 
     public void run() {
 
-		boolean gotByePacket = false;
+	boolean gotByePacket = false;
 		
-		try {
-			/* stream to read from client */
-			ObjectInputStream fromClient = new ObjectInputStream(socket.getInputStream());
-			BrokerPacket packetFromClient;
+	try {
+	    /* stream to read from client */
+	    ObjectInputStream fromClient = new ObjectInputStream(socket.getInputStream());
+	    BrokerPacket packetFromClient;
 			
-			/* stream to write back to client */
-			ObjectOutputStream toClient = new ObjectOutputStream(socket.getOutputStream());
+	    /* stream to write back to client */
+	    ObjectOutputStream toClient = new ObjectOutputStream(socket.getOutputStream());
 			
 
-			while ((packetFromClient = (BrokerPacket) fromClient.readObject()) != null) {
-				/* create a packet to send reply back to client */
-			    	BrokerPacket packetToClient = new BrokerPacket();
+	    while ((packetFromClient = (BrokerPacket) fromClient.readObject()) != null) {
+		/* create a packet to send reply back to client */
+		BrokerPacket packetToClient = new BrokerPacket();
 
-				/* BROKER_REQUEST */
-				if(packetFromClient.type == BrokerPacket.BROKER_REQUEST) {
-					System.out.println("From Client: " + packetFromClient.symbol);
-					if (packetFromClient.symbol == null || !table.containsKey(packetFromClient.symbol)) {
-						/* valid symbol could not be processed */
-						System.out.println("From Client: Could not find symbol");
-						System.out.println(table.toString());
-						System.out.println(table.get(packetFromClient.symbol));
-					     
-						// Search other brokers if they have it
-						System.out.println("Find symbol via lookup."); 
-		
-						/* make a new request packet */
-						BrokerPacket packetToLookup = new BrokerPacket();
-						packetToLookup.type = BrokerPacket.BROKER_FORWARD;
-						packetToLookup.symbol = packetFromClient.symbol;
+		/* BROKER_FORWARD */
+		if(packetFromClient.type == BrokerPacket.BROKER_FORWARD) {				    	
+		    System.out.println("From another broker: " + packetFromClient.symbol);
 
-						lookupout.writeObject(packetToLookup);
+		    if (packetFromClient.symbol == null || !table.containsKey(packetFromClient.symbol)) {
+			/* valid symbol could not be processed */
+			System.out.println("From Client: request error");
+			System.out.println(table.toString());
+			System.out.println(table.get(packetFromClient.symbol));
+			packetToClient.type = BrokerPacket.ERROR_INVALID_SYMBOL;
+		    } else {
+			packetToClient.type = BrokerPacket.BROKER_QUOTE;
+			System.out.println("Replying to Client: " + table.get(packetFromClient.symbol));
+			packetToClient.quote = table.get(packetFromClient.symbol);
+		    }
 
-
-
-						packetToClient.type = BrokerPacket.ERROR_INVALID_SYMBOL;
-					} else {
-						packetToClient.type = BrokerPacket.BROKER_QUOTE;
-
-						System.out.println("Replying to Client: " + table.get(packetFromClient.symbol));
-						packetToClient.quote = table.get(packetFromClient.symbol);
-					}
-					toClient.writeObject(packetToClient);
-					continue;
-				}
-				
-				/* BROKER_NULL || BROKER_BYE */
-				if (packetFromClient.type == BrokerPacket.BROKER_NULL || packetFromClient.type == BrokerPacket.BROKER_BYE) {
-					gotByePacket = true;
-					packetToClient.type = BrokerPacket.BROKER_BYE;
-					System.out.println("Client is exiting");
-					toClient.writeObject(packetToClient);
-					break;
-				}
-
-             	   		/* EXCHANGE_ADD */
-				if (packetFromClient.type == BrokerPacket.EXCHANGE_ADD) {
-				    	packetToClient.type = BrokerPacket.EXCHANGE_REPLY;
-
-					System.out.println("From Client: EXCHANGE_ADD ");
-					System.out.println("From Client: " + packetFromClient.symbol);
-
-				 	if (table.get(packetFromClient.symbol) != null) {
-						System.out.println("ERROR: symbol already exists");
-						packetToClient.error_code = BrokerPacket.ERROR_SYMBOL_EXISTS;
-				    	} else {
-						table.put(packetFromClient.symbol, Long.valueOf(0));
-						OnlineBrokerHandlerThread.updateTable();
-
-						System.out.println("To Client: add success ");
-						packetToClient.symbol = packetFromClient.symbol;
-						packetToClient.error_code = 0;
-					}
-
-					toClient.writeObject(packetToClient);
-				    	continue;
-				}
-
-				/* EXCHANGE_UPDATE */
-				if (packetFromClient.type == BrokerPacket.EXCHANGE_UPDATE) {
-				    	packetToClient.type = BrokerPacket.EXCHANGE_REPLY;
-
-					System.out.println("From Client: EXCHANGE_UPDATE ");
-					System.out.println("From Client: " + packetFromClient.symbol);
-					System.out.println("From Client: " + packetFromClient.quote);
-
-					if (table.get(packetFromClient.symbol) == null) {
-					    System.out.println("ERROR: symbol does not exist");
-					    packetToClient.error_code = BrokerPacket.ERROR_INVALID_SYMBOL;
-					} else if (packetFromClient.quote > 300 || packetFromClient.quote < 1) {
-					    System.out.println("ERROR: quote out of range ");
-					    packetToClient.error_code = BrokerPacket.ERROR_OUT_OF_RANGE;
-					} else {
-					    table.put(packetFromClient.symbol, packetFromClient.quote);
-					    OnlineBrokerHandlerThread.updateTable();
-					    System.out.println("To Client: update success ");
-					    packetToClient.error_code = 0;
-					    packetToClient.quote = packetFromClient.quote;
-					}
-					toClient.writeObject(packetToClient);
-					continue;
-				}
-
-				/* EXCHANGE_REMOVE */
-				if (packetFromClient.type == BrokerPacket.EXCHANGE_REMOVE) {
-				    packetToClient.type = BrokerPacket.EXCHANGE_REPLY;
-
-				    System.out.println("From Client: EXCHANGE_REMOVE ");
-				    System.out.println("From Client: " + packetFromClient.symbol);
-
-				    if (table.get(packetFromClient.symbol) == null) {
-					System.out.println("ERROR: symbol does not exist");
-					packetToClient.error_code = BrokerPacket.ERROR_INVALID_SYMBOL;
-				    } else {
-					table.remove(packetFromClient.symbol);
-					OnlineBrokerHandlerThread.updateTable();
-					System.out.println("To Client: remove success ");
-					packetToClient.error_code = 0;
-				    }
-				    toClient.writeObject(packetToClient);
-				    continue;
-				}
-
-				/* if code comes here, there is an error in the packet */
-				System.err.println("ERROR: Unknown packet!!");
-				System.exit(-1);
-			}
-			
-			/* cleanup when client exits */
-			fromClient.close();
-			toClient.close();
-			socket.close();
-
-		} catch (IOException e) {
-			if(!gotByePacket)
-				e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			if(!gotByePacket)
-				e.printStackTrace();
+		    toClient.writeObject(packetToClient);
+		    continue;
 		}
+
+		/* BROKER_REQUEST */
+		if(packetFromClient.type == BrokerPacket.BROKER_REQUEST) {
+		    System.out.println("From Client: " + packetFromClient.symbol);
+		    if (packetFromClient.symbol == null || !table.containsKey(packetFromClient.symbol)) {
+			/* valid symbol could not be processed */
+			System.out.println("From Client: Could not find symbol");
+			System.out.println(table.toString());
+			System.out.println(table.get(packetFromClient.symbol));
+					     
+			// Search other brokers if they have it
+			System.out.println("Find symbol in other brokers via Lookup."); 
+		
+			/* make a new request packet */
+			BrokerPacket packetToLookup = new BrokerPacket();
+			packetToLookup.type = BrokerPacket.BROKER_FORWARD;
+			packetToLookup.symbol = packetFromClient.symbol;
+
+			lookupout.writeObject(packetToLookup);
+
+			// Get reply from lookup
+			BrokerPacket packetFromLookup;
+			packetFromLookup = (BrokerPacket) lookupin.readObject();
+	  	
+			boolean resultFound  = false;
+
+			if(packetFromLookup.type == BrokerPacket.LOOKUP_REPLY){
+			    // Traverse brokers until until symbol is found
+			    int num_locations = packetFromLookup.num_locations;
+
+			    for(int i = 0; i < num_locations; i++){						    
+				// Connect to brokers.
+
+				// Temp variables
+				Socket temp_brokerSocket = null;
+				ObjectOutputStream temp_out = null;
+				ObjectInputStream temp_in = null;
+
+				String temp_hostname = null;
+				int temp_port = -1;
+
+				temp_hostname = packetFromLookup.locations[0].broker_host;
+				temp_port = packetFromLookup.locations[0].broker_port;	
+
+				// Connect to broker
+				try {
+				    temp_brokerSocket = new Socket(temp_hostname, temp_port);
+
+				    temp_out = new ObjectOutputStream(temp_brokerSocket.getOutputStream());
+				    temp_in = new ObjectInputStream(temp_brokerSocket.getInputStream());
+
+				} catch (UnknownHostException e) {
+				    System.err.println("ERROR: Don't know where to connect!!");
+				    //System.exit(1);
+				} catch (IOException e) {
+				    System.err.println("ERROR: Couldn't get I/O for the connection.");
+				    //System.exit(1);
+				}
+
+				// Request BROKER_FORWARD to broker
+				/* make a new request packet */
+				BrokerPacket packetToBroker = new BrokerPacket();
+				packetToBroker.type = BrokerPacket.BROKER_FORWARD;
+				packetToBroker.symbol = packetFromClient.symbol;
+
+				temp_out.writeObject(packetToBroker);
+
+				// Get reply from lookup
+				BrokerPacket packetFromBroker;
+				packetFromBroker = (BrokerPacket) temp_in.readObject();
+						    
+				if(packetFromBroker.type  == BrokerPacket.BROKER_QUOTE){
+				    resultFound = true;
+				    packetToClient.quote = packetFromBroker.quote;
+				    break;
+				}
+						
+
+			    }
+			}
+
+			if(resultFound)
+			    packetToClient.type = BrokerPacket.BROKER_QUOTE;	
+			else
+			    packetToClient.type = BrokerPacket.ERROR_INVALID_SYMBOL;
+
+					       
+		    } else {
+			packetToClient.type = BrokerPacket.BROKER_QUOTE;
+
+			System.out.println("Replying to Client: " + table.get(packetFromClient.symbol));
+			packetToClient.quote = table.get(packetFromClient.symbol);
+		    }
+		    toClient.writeObject(packetToClient);
+		    continue;
+		}
+				
+		/* BROKER_NULL || BROKER_BYE */
+		if (packetFromClient.type == BrokerPacket.BROKER_NULL || packetFromClient.type == BrokerPacket.BROKER_BYE) {
+		    gotByePacket = true;
+		    packetToClient.type = BrokerPacket.BROKER_BYE;
+		    System.out.println("Client is exiting");
+		    toClient.writeObject(packetToClient);
+		    break;
+		}
+
+		/* EXCHANGE_ADD */
+		if (packetFromClient.type == BrokerPacket.EXCHANGE_ADD) {
+		    packetToClient.type = BrokerPacket.EXCHANGE_REPLY;
+
+		    System.out.println("From Client: EXCHANGE_ADD ");
+		    System.out.println("From Client: " + packetFromClient.symbol);
+
+		    if (table.get(packetFromClient.symbol) != null) {
+			System.out.println("ERROR: symbol already exists");
+			packetToClient.error_code = BrokerPacket.ERROR_SYMBOL_EXISTS;
+		    } else {
+			table.put(packetFromClient.symbol, Long.valueOf(0));
+			OnlineBrokerHandlerThread.updateTable();
+
+			System.out.println("To Client: add success ");
+			packetToClient.symbol = packetFromClient.symbol;
+			packetToClient.error_code = 0;
+		    }
+
+		    toClient.writeObject(packetToClient);
+		    continue;
+		}
+
+		/* EXCHANGE_UPDATE */
+		if (packetFromClient.type == BrokerPacket.EXCHANGE_UPDATE) {
+		    packetToClient.type = BrokerPacket.EXCHANGE_REPLY;
+
+		    System.out.println("From Client: EXCHANGE_UPDATE ");
+		    System.out.println("From Client: " + packetFromClient.symbol);
+		    System.out.println("From Client: " + packetFromClient.quote);
+
+		    if (table.get(packetFromClient.symbol) == null) {
+			System.out.println("ERROR: symbol does not exist");
+			packetToClient.error_code = BrokerPacket.ERROR_INVALID_SYMBOL;
+		    } else if (packetFromClient.quote > 300 || packetFromClient.quote < 1) {
+			System.out.println("ERROR: quote out of range ");
+			packetToClient.error_code = BrokerPacket.ERROR_OUT_OF_RANGE;
+		    } else {
+			table.put(packetFromClient.symbol, packetFromClient.quote);
+			OnlineBrokerHandlerThread.updateTable();
+			System.out.println("To Client: update success ");
+			packetToClient.error_code = 0;
+			packetToClient.quote = packetFromClient.quote;
+		    }
+		    toClient.writeObject(packetToClient);
+		    continue;
+		}
+
+		/* EXCHANGE_REMOVE */
+		if (packetFromClient.type == BrokerPacket.EXCHANGE_REMOVE) {
+		    packetToClient.type = BrokerPacket.EXCHANGE_REPLY;
+
+		    System.out.println("From Client: EXCHANGE_REMOVE ");
+		    System.out.println("From Client: " + packetFromClient.symbol);
+
+		    if (table.get(packetFromClient.symbol) == null) {
+			System.out.println("ERROR: symbol does not exist");
+			packetToClient.error_code = BrokerPacket.ERROR_INVALID_SYMBOL;
+		    } else {
+			table.remove(packetFromClient.symbol);
+			OnlineBrokerHandlerThread.updateTable();
+			System.out.println("To Client: remove success ");
+			packetToClient.error_code = 0;
+		    }
+		    toClient.writeObject(packetToClient);
+		    continue;
+		}
+
+		/* if code comes here, there is an error in the packet */
+		System.err.println("ERROR: Unknown packet!!");
+		System.exit(-1);
+	    }
+			
+	    /* cleanup when client exits */
+	    fromClient.close();
+	    toClient.close();
+	    socket.close();
+
+	} catch (IOException e) {
+	    if(!gotByePacket)
+		e.printStackTrace();
+	} catch (ClassNotFoundException e) {
+	    if(!gotByePacket)
+		e.printStackTrace();
+	}
     }
 
     /* Accessors */
