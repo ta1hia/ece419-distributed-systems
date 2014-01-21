451c6a292d84505e78a214213d325ab8b5fd3338
Tahia Khan <tahia.khan@gmail.com>
2014-01-20 21:18:04 -0500

    Fixing exiting and local cmd error checking

--- labs/lab1/broker/broker3/BrokerClient.java
+++ labs/lab1/broker/broker3/BrokerClient.java
@@ -2,157 +2,157 @@ import java.io.*;
 import java.net.*;
 
 public class BrokerClient {
-	public static void main(String[] args) throws IOException,
-			ClassNotFoundException {
+    public static void main(String[] args) throws IOException,
+           ClassNotFoundException {
 
-	    	// Connect to lookup 
-		Socket lookupSocket = null;
-		ObjectOutputStream lookupout = null;
-		ObjectInputStream lookupin = null;
-		
-		String lookupHostname = null;
-		int lookupPort = -1;
+               // Connect to lookup 
+               Socket lookupSocket = null;
+               ObjectOutputStream lookupout = null;
+               ObjectInputStream lookupin = null;
 
-		// Get IP and port from lookup		
-		Socket brokerSocket = null;
-		ObjectOutputStream out = null;
-		ObjectInputStream in = null;
+               String lookupHostname = null;
+               int lookupPort = -1;
 
-		try {
-			/* variables for hostname/port */
-		    	// Don't hardwire!
-			
-			if(args.length == 2) {
-				lookupHostname = args[0];
-				lookupPort = Integer.parseInt(args[1]);
-			} else {
-				System.err.println("ERROR: Invalid arguments!");
-				System.exit(-1);
-			}
-			lookupSocket = new Socket(lookupHostname, lookupPort);
+               // Get IP and port from lookup		
+               Socket brokerSocket = null;
+               ObjectOutputStream out = null;
+               ObjectInputStream in = null;
 
-			lookupout = new ObjectOutputStream(lookupSocket.getOutputStream());
-			lookupin = new ObjectInputStream(lookupSocket.getInputStream());
+               try {
+                   /* variables for hostname/port */
+                   // Don't hardwire!
 
-		} catch (UnknownHostException e) {
-			System.err.println("ERROR: Don't know where to connect!!");
-			System.exit(1);
-		} catch (IOException e) {
-			System.err.println("ERROR: Couldn't get I/O for the connection.");
-			System.exit(1);
-		}
+                   if(args.length == 2) {
+                       lookupHostname = args[0];
+                       lookupPort = Integer.parseInt(args[1]);
+                   } else {
+                       System.err.println("ERROR: Invalid arguments!");
+                       System.exit(-1);
+                   }
+                   lookupSocket = new Socket(lookupHostname, lookupPort);
 
-		// Broker variables
-		String hostname = null;
-		int port = -1;
+                   lookupout = new ObjectOutputStream(lookupSocket.getOutputStream());
+                   lookupin = new ObjectInputStream(lookupSocket.getInputStream());
 
-		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-		String userInput;
+               } catch (UnknownHostException e) {
+                   System.err.println("ERROR: Don't know where to connect!!");
+                   System.exit(1);
+               } catch (IOException e) {
+                   System.err.println("ERROR: Couldn't get I/O for the connection.");
+                   System.exit(1);
+               }
 
-		System.out.print("Enter queries or quit for exit:\n");
-		System.out.print("> ");
+               // Broker variables
+               String hostname = null;
+               int port = -1;
 
-		while ((userInput = stdIn.readLine()) != null && userInput.toLowerCase().indexOf("x") == -1) {
-			
-                        // Split strings into seperate parts
-                         String parts[] = userInput.split(" ");
+               BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+               String userInput;
 
-                        /* make a new request packet */
-                        String command = parts[0].toLowerCase();
+               System.out.print("Enter queries or quit for exit:\n");
+               System.out.print("> ");
 
-		    	/* make a new request packet */
-		    	if(command.equals("local")){ // Check if it's a request to lookup
-			    String symbol = parts[1].toLowerCase();
+               while ((userInput = stdIn.readLine()) != null && userInput.toLowerCase().indexOf("x") == -1) {
 
-			    // Make a lookup packet request
-			    BrokerPacket packetToLookup = new BrokerPacket();
-			    packetToLookup.type = BrokerPacket.LOOKUP_REQUEST;
-			    packetToLookup.symbol = symbol;
-			    lookupout.writeObject(packetToLookup);
+                   // Split strings into seperate parts
+                   String parts[] = userInput.split(" ");
 
-			    // Get reply from lookup
-			    BrokerPacket packetFromLookup;
-			    packetFromLookup = (BrokerPacket) lookupin.readObject();
-	  	
-			    /* global variables for hostname/port */
-			    hostname = packetFromLookup.locations[0].broker_host;
-			    port = packetFromLookup.locations[0].broker_port;	
+                   /* make a new request packet */
+                   String command = parts[0].toLowerCase();
 
-			    // Connect to broker
-			    try {
-				brokerSocket = new Socket(hostname, port);
+                   /* make a new request packet */
+                   if(command.equals("local")){ // Check if it's a request to lookup
+                       String symbol = parts[1].toLowerCase();
 
-				out = new ObjectOutputStream(brokerSocket.getOutputStream());
-				in = new ObjectInputStream(brokerSocket.getInputStream());
+                       // Make a lookup packet request
+                       BrokerPacket packetToLookup = new BrokerPacket();
+                       packetToLookup.type = BrokerPacket.LOOKUP_REQUEST;
+                       packetToLookup.symbol = symbol;
+                       lookupout.writeObject(packetToLookup);
 
-			    } catch (UnknownHostException e) {
-				System.err.println("ERROR: Don't know where to connect!!");
-				System.exit(1);
-			    } catch (IOException e) {
-				System.err.println("ERROR: Couldn't get I/O for the connection.");
-				System.exit(1);
-			    }
+                       // Get reply from lookup
+                       BrokerPacket packetFromLookup;
+                       packetFromLookup = (BrokerPacket) lookupin.readObject();
 
-			    
-			    System.out.print(symbol + " as local.\n");
-			    /* re-print console prompt */
-			    System.out.print("> ");
+                       /* global variables for hostname/port */
+                       hostname = packetFromLookup.locations[0].broker_host;
+                       port = packetFromLookup.locations[0].broker_port;	
 
-			    continue;
-			}
+                       // Connect to broker
+                       try {
+                           brokerSocket = new Socket(hostname, port);
 
-			if(hostname == null || port < 0) { // Client is currently not connected
-			            System.out.print("Client is currently not connected to a broker... Use command 'local'.");			    	
-			}
+                           out = new ObjectOutputStream(brokerSocket.getOutputStream());
+                           in = new ObjectInputStream(brokerSocket.getInputStream());
 
-			// This is a request to broker	
-			BrokerPacket packetToServer = new BrokerPacket();
-			packetToServer.type = BrokerPacket.BROKER_REQUEST;
-			packetToServer.symbol = userInput.toLowerCase();
-			out.writeObject(packetToServer);
-			
+                       } catch (UnknownHostException e) {
+                           System.err.println("ERROR: Don't know where to connect!!");
+                           System.exit(1);
+                       } catch (IOException e) {
+                           System.err.println("ERROR: Couldn't get I/O for the connection.");
+                           System.exit(1);
+                       }
 
-			/* print server reply */
-			BrokerPacket packetFromServer;
-			packetFromServer = (BrokerPacket) in.readObject();
+                       System.out.print(symbol + " as local.\n");
+                       /* re-print console prompt */
+                       System.out.print("> ");
+
+                       continue;
+                   }
+
+                   if(hostname == null || port < 0) { // Client is currently not connected
+                       System.out.println("Client is currently not connected to a broker... Use command 'local'.");			    	
+                       System.out.print("> ");
+                       continue;
+                   }
+
+                   // This is a request to broker	
+                   BrokerPacket packetToServer = new BrokerPacket();
+                   packetToServer.type = BrokerPacket.BROKER_REQUEST;
+                   packetToServer.symbol = userInput.toLowerCase();
+                   out.writeObject(packetToServer);
 
 
-			if (packetFromServer.type == BrokerPacket.BROKER_QUOTE){
-				int isError = packetFromServer.error_code;
-
-                                switch(isError) {
-                                        case BrokerPacket.ERROR_INVALID_SYMBOL:         System.out.print(packetFromServer.symbol + " invalid.");
-                                                                        continue;
-                                        case BrokerPacket.ERROR_OUT_OF_RANGE:         System.out.print(packetFromServer.symbol + " out of range.");
-                                                                        continue;
-                                        case BrokerPacket.ERROR_SYMBOL_EXISTS:         System.out.print(packetFromServer.symbol + " exists.");
-                                                                        continue;        
-                                        case BrokerPacket.ERROR_INVALID_EXCHANGE:         System.out.print(packetFromServer.symbol + " invalid.");
-                                                                        continue;   
-                                        case 0: System.out.println("Quote from broker: " + String.valueOf(packetFromServer.quote));
-                                        default: break; 
-                                }
-
-				
-			} else {
-                		/* error returned - this case isn't handled in Broker1 */
-                		System.out.println("Quote from broker: 0");
-            		}
+                   /* print server reply */
+                   BrokerPacket packetFromServer;
+                   packetFromServer = (BrokerPacket) in.readObject();
 
 
-			/* re-print console prompt */
-			System.out.print("> ");
-		}
+                   if (packetFromServer.type == BrokerPacket.BROKER_QUOTE){
+                       int isError = packetFromServer.error_code;
 
-		/* tell server that i'm quitting */
-		BrokerPacket packetToServer = new BrokerPacket();
-		packetToServer.type = BrokerPacket.BROKER_BYE;
-		//packetToServer.message = "Bye!";
-		out.writeObject(packetToServer);
+                       switch(isError) {
+                           case BrokerPacket.ERROR_INVALID_SYMBOL:         System.out.print(packetFromServer.symbol + " invalid.");
+                                                                           continue;
+                           case BrokerPacket.ERROR_OUT_OF_RANGE:         System.out.print(packetFromServer.symbol + " out of range.");
+                                                                         continue;
+                           case BrokerPacket.ERROR_SYMBOL_EXISTS:         System.out.print(packetFromServer.symbol + " exists.");
+                                                                          continue;        
+                           case BrokerPacket.ERROR_INVALID_EXCHANGE:         System.out.print(packetFromServer.symbol + " invalid.");
+                                                                             continue;   
+                           case 0: System.out.println("Quote from broker: " + String.valueOf(packetFromServer.quote));
+                           default: break; 
+                       }
+                   } else {
+                       /* error returned - this case isn't handled in Broker1 */
+                       System.out.println("Quote from broker: 0");
+                   }
 
-		out.close();
-		in.close();
-		stdIn.close();
-		brokerSocket.close();
-	}
+                   /* re-print console prompt */
+                   System.out.print("> ");
+               }
+
+               /* tell server that i'm quitting */
+               if (out != null) {
+                   System.out.println("YO");
+                   BrokerPacket packetToServer = new BrokerPacket();
+                   packetToServer.type = BrokerPacket.BROKER_BYE;
+                   out.writeObject(packetToServer);
+                   out.close();
+                   in.close();
+                   brokerSocket.close();
+               }
+
+               stdIn.close();
+           }
 }
