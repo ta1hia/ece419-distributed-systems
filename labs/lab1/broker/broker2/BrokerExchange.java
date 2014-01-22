35b3b5fcb8d37a54f3a4f645a58194b7b6c89287
Tahia Khan <tahia.khan@gmail.com>
2014-01-22 13:01:44 -0500

    Adding some io error checking to pt 2

--- labs/lab1/broker/broker2/BrokerExchange.java
+++ labs/lab1/broker/broker2/BrokerExchange.java
@@ -3,120 +3,128 @@ import java.net.*;
 import java.util.Arrays;
 
 public class BrokerExchange {
-	public static void main(String[] args) throws IOException,
-			ClassNotFoundException {
+    public static void main(String[] args) throws IOException,
+           ClassNotFoundException {
 
-		Socket brokerSocket = null;
-		ObjectOutputStream out = null;
-		ObjectInputStream in = null;
+               Socket brokerSocket = null;
+               ObjectOutputStream out = null;
+               ObjectInputStream in = null;
 
-		try {
-			// /* variables for hostname/port */ Don't hardwire!
-			String hostname = null;
-			int port = -1;
-			
-			if(args.length == 2) {
-				hostname = args[0];
-				port = Integer.parseInt(args[1]);
-			} else {
-				System.err.println("ERROR: Invalid arguments!");
-				System.exit(-1);
-			}
-			brokerSocket = new Socket(hostname, port);
+               try {
+                   /* variables for hostname/port */
+                   String hostname = "localhost";
+                   int port = 4444;
 
-			out = new ObjectOutputStream(brokerSocket.getOutputStream());
-			in = new ObjectInputStream(brokerSocket.getInputStream());
+                   if(args.length == 2) {
+                       hostname = args[0];
+                       port = Integer.parseInt(args[1]);
+                   } else {
+                       System.err.println("ERROR: Invalid arguments!");
+                       System.exit(-1);
+                   }
+                   brokerSocket = new Socket(hostname, port);
 
-		} catch (UnknownHostException e) {
-			System.err.println("ERROR: Don't know where to connect!!");
-			System.exit(1);
-		} catch (IOException e) {
-			System.err.println("ERROR: Couldn't get I/O for the connection.");
-			System.exit(1);
-		}
+                   out = new ObjectOutputStream(brokerSocket.getOutputStream());
+                   in = new ObjectInputStream(brokerSocket.getInputStream());
 
-		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-		String userInput;
+               } catch (UnknownHostException e) {
+                   System.err.println("ERROR: Don't know where to connect!!");
+                   System.exit(1);
+               } catch (IOException e) {
+                   System.err.println("ERROR: Couldn't get I/O for the connection.");
+                   System.exit(1);
+               }
 
-		System.out.print("Enter command or quit for exit:\n");
-		System.out.print("> ");
+               BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+               String userInput;
 
-		while ((userInput = stdIn.readLine()) != null && !userInput.toLowerCase().equals("quit") && !userInput.toLowerCase().equals("x")) {
-	
-			// Split strings into seperate parts
- 			String parts[] = userInput.split(" ");
+               System.out.print("Enter command or quit for exit:\n");
+               System.out.print("> ");
 
-			/* make a new request packet */
-			BrokerPacket packetToServer = new BrokerPacket();
-			String command = parts[0].toLowerCase();
-			String originalSymbol = parts[1];
-			String symbol = parts[1].toLowerCase();
+               while ((userInput = stdIn.readLine()) != null && userInput.toLowerCase().indexOf("quit") == -1) {
 
-			/* Check what type of request it is. */
-            		if (command.equals("add")) {
-				packetToServer.type = BrokerPacket.EXCHANGE_ADD;
-			} else if (command.equals("update")) {
-			    	packetToServer.type = BrokerPacket.EXCHANGE_UPDATE;
-				    packetToServer.quote= Long.parseLong(parts[2], 10);
-			} else if (command.equals("remove")) {
-			    	packetToServer.type = BrokerPacket.EXCHANGE_REMOVE;
-			} else {
-				System.out.print("Unknown command...\n");
+                   BrokerPacket packetToServer = null;
+                   String command = null;
+                   String symbol = null;
+                   String parts[] = userInput.split(" ");
 
-				/* re-print console prompt */
-				System.out.print("> ");
+                   try {	
+                       // Split strings into seperate parts
 
-				continue;			
-			}
+                       /* make a new request packet */
+                       packetToServer = new BrokerPacket();
+                       command = parts[0].toLowerCase();
+                       symbol = parts[1].toLowerCase();
+                   } catch (Exception e) {
+                       System.out.print("Invalid arguments...\n> ");
+                       continue;
+                   }
 
+                   /* Check what type of request it is. */
+                   if (command.equals("add")) {
+                       packetToServer.type = BrokerPacket.EXCHANGE_ADD;
+                   } else if (command.equals("update")) {
+                       packetToServer.type = BrokerPacket.EXCHANGE_UPDATE;
+                       packetToServer.quote= Long.parseLong(parts[2], 10);
+                   } else if (command.equals("remove")) {
+                       packetToServer.type = BrokerPacket.EXCHANGE_REMOVE;
+                   } else {
+                       System.out.print("Unknown command...\n");
 
-			packetToServer.symbol = symbol;
-			out.writeObject(packetToServer);
+                       /* re-print console prompt */
+                       System.out.print("> ");
 
-			/* print server reply */
-			BrokerPacket packetFromServer;
-			packetFromServer = (BrokerPacket) in.readObject();
-
-			if (packetFromServer.type == BrokerPacket.EXCHANGE_REPLY){
-				int isError = packetFromServer.error_code;
-
-				switch(isError) {
-					case BrokerPacket.ERROR_INVALID_SYMBOL: 	System.out.print(originalSymbol + " invalid.\n> ");
-									continue;
-					case BrokerPacket.ERROR_OUT_OF_RANGE: 	System.out.print(originalSymbol + " out of range.\n> ");
-									continue;
-					case BrokerPacket.ERROR_SYMBOL_EXISTS: 	System.out.print(originalSymbol + " exists.\n> ");
-									continue;	
-					case BrokerPacket.ERROR_INVALID_EXCHANGE: 	System.out.print(originalSymbol + " invalid.\n> ");
-									continue;	
-					default: break; 
-				}
-                
-                if (command.equals("add")) {
-                    System.out.print(originalSymbol + " added.\n");
-                } else if (command.equals("update")) {
-					System.out.print(originalSymbol + " updated to " + String.valueOf(packetFromServer.quote) + ".\n");
-			 	} else if (command.equals("remove")) {
-					System.out.print(originalSymbol + " removed.\n");
-				} else {
-					System.out.print("Unknown command...\n");
-			    	}
-			}
+                       continue;			
+                   }
 
 
-			/* re-print console prompt */
-			System.out.print("> ");
-		}
+                   packetToServer.symbol = symbol;
+                   out.writeObject(packetToServer);
 
-		/* tell server that i'm quitting */
-		BrokerPacket packetToServer = new BrokerPacket();
-		packetToServer.type = BrokerPacket.BROKER_BYE;
-		//packetToServer.message = "Bye!";
-		out.writeObject(packetToServer);
+                   /* print server reply */
+                   BrokerPacket packetFromServer;
+                   packetFromServer = (BrokerPacket) in.readObject();
 
-		out.close();
-		in.close();
-		stdIn.close();
-		brokerSocket.close();
-	}
+                   if (packetFromServer.type == BrokerPacket.EXCHANGE_REPLY){
+                       int isError = packetFromServer.error_code;
+
+                       switch(isError) {
+                           case BrokerPacket.ERROR_INVALID_SYMBOL: 	System.out.print(symbol + " invalid.\n");
+                                                                    continue;
+                           case BrokerPacket.ERROR_OUT_OF_RANGE: 	System.out.print(symbol + " out of range.\n");
+                                                                    continue;
+                           case BrokerPacket.ERROR_SYMBOL_EXISTS: 	System.out.print(symbol + " exists.\n");
+                                                                    continue;	
+                           case BrokerPacket.ERROR_INVALID_EXCHANGE: 	System.out.print(symbol + " invalid.\n");
+                                                                        continue;	
+                           default: break; 
+                       }
+
+                       if (command.equals("add")) {
+                           System.out.print(symbol + " added.\n");
+                       } else if (command.equals("update")) {
+                           System.out.print(symbol + " updated to " + String.valueOf(packetFromServer.quote) + ".\n");
+                       } else if (command.equals("remove")) {
+                           System.out.print(symbol + " removed.\n");
+                       } else {
+                           System.out.print("Unknown command...\n");
+                       }
+                   }
+
+
+                   /* re-print console prompt */
+                   System.out.print("> ");
+               }
+
+               /* tell server that i'm quitting */
+               BrokerPacket packetToServer = new BrokerPacket();
+               packetToServer.type = BrokerPacket.BROKER_BYE;
+               //packetToServer.message = "Bye!";
+               out.writeObject(packetToServer);
+
+               out.close();
+               in.close();
+               stdIn.close();
+               brokerSocket.close();
+           }
 }
