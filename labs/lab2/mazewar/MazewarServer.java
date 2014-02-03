183b091e88e936e4c860e0ec1d98abfff2e9552e
Tahia Khan <tahia.khan@gmail.com>
2014-02-03 13:21:19 -0500

    Adding basic server code to MazewarServer

--- labs/lab2/mazewar/MazewarServer.java
+++ labs/lab2/mazewar/MazewarServer.java
@@ -1,3 +1,4 @@
+import java.net.ServerSocket;
 
 
 /* MazewarServer class
@@ -7,4 +8,29 @@
  *
  */
 public class MazewarServer {
+
+    public static void main(String[] args) throws IOException {
+        SocketServer mazewarServer = null;
+        boolean listening = true;
+
+        /* Create MazewarServer socket */
+        try { 
+            if (args.length == 1) {
+                brokerSocket = new ServerSocket(Integer.parseInt(args[0]));
+            } else {
+                System.err.println("Error: Invalid arguments!");
+                System.exit(-1);
+            }
+        } catch (IOException e) {
+            System.err.println("ERROR: Could not listen on port!");
+            System.exit(-1);
+        }
+    }
+
+    /* Listen for new remote clients */
+    while (listening) {
+        //new MazewarServerHandlerThread();
+    }
+
+    mazewarServer.close();
 }
