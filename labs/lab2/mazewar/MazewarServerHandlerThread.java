8afe01e51646d7112f7a05a6096b9800dd0761c8
Tahia Khan <tahia.khan@gmail.com>
2014-02-03 14:57:40 -0500

    Adding server handler skeleton stuff

--- labs/lab2/mazewar/MazewarServerHandlerThread.java
+++ labs/lab2/mazewar/MazewarServerHandlerThread.java
@@ -1,4 +1,6 @@
-
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
 
 /* MazewarServerHandlerThread class
  *
@@ -7,5 +9,30 @@
  */
 
 public class MazewarServerHandlerThread extends Thread {
+    Socket rc_socket = null;
+
+    DataInputStream cin;
+    DataOutputStream cout;
+
+    public MazewarServerHandlerThread (Socket socket) {
+        super("MazewarServerHandlerThread");
+        rc_socket = socket;
+        cin = new DataInputStream(rc_socket.getInputStream());
+        cout = new DataOutputStream(rc_socket.getOutputStream());
+        System.out.println("Created new MazewarServerHandlerThread to handle remote client ");
+    }
+
+    public void run() {
+        /* Wait for handshaking packet from client, store client state in 
+         * global client table */
+
+
+        /* Send game state to client */
+
+        /* Loop: 
+         * Wait for client event
+         * Add event to global event queue
+         */
+    }
 
 }
