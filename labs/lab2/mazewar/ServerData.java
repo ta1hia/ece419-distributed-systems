2814acb4e4a4aa974ce8c8f015b3614dffa65595
Tahia Khan <tahia.khan@gmail.com>
2014-02-07 16:51:24 -0500

    Adding ServerData.java to dependency

---
+++ labs/lab2/mazewar/ServerData.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class ServerData implements Serializable {
+    //eventqueue
+    //clientqueue
+    BlockingQueue<MazeEvent> eventQueue = new LinkedBlockingQueue();
+    ConcurrentHashMap<String, Point> clientTable = new ConcurrentHashMap<>(); //Might need reference to actual thread here, for dispatcher
+
+    public void addClientToTable(String name, Point position) {
+        if (!clientTable.containsKey(name)) {
+            clientTable.put(name, position);
+        } else {
+            System.out.println("Client with name " + name + " already exists.");
+        }
+
+    }
+}
