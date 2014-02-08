c4aed15aa2f7d2baf4c5882ba2e975fee25ec67b
Gumba  Kevin Justin <gumbakev@ug207.eecg>
2014-02-07 23:53:56 -0500

    Modifying broadcast. Commented out.

--- labs/lab2/mazewar/Dispatcher.java
+++ labs/lab2/mazewar/Dispatcher.java
@@ -1,10 +1,40 @@
 
+// import java.util.concurrent.ConcurrentHashMap;
+// import java.util.concurrent.LinkedBlockingQueue;
 
-/* Dispatcher class
- * Dispatches messages from event queue and broadcasts
- * events to all remote clients.
- *
- */
-public class Dispatcher extends Thread {
-}
+// /* Dispatcher class
+//  * Dispatches messages from event queue and broadcasts
+//  * events to all remote clients.
+//  *
+//  */
+// public class Dispatcher extends Thread {
+//     ConcurrentHashMap <String,String> clientTable;
+//     LinkedBlockingQueue eventQueue;
+
+//     // Continually check eventqueue
+//     // Broadcast whenever queue is not empty
+//     public void run(){
+// 	String new_event = null;
+// 	int seqNum = 0;
+
+// 	while(true){ //???
+// 	    if(new_event = eventQueue.peek() != null){
+// 		// Go through each client	    
+// 		for(int i=0;i < clientTable.size()){
+// 		    clientTable.get(i).send(seqNum, command);
+// 		}
+// 		seqNum++;
+// 		if(seqNum == 501)
+// 		    seqNum = 0;
+// 	    }
+// 	}
+
+//     }
+
+//     public static void setTable (ConcurrentHashMap <String,String> clientTable, LinkedBlockingQueue eventQueue){
+// 	Dispatcher.eventQueue = eventQueue;
+// 	Dispatcher.clientTable = clientTable;
+//     }
+
+// }
 
