ea42de516011a9fa17f42ba9990d4923974ca3e7
Gumba  Kevin Justin <gumbakev@ug207.eecg>
2014-02-08 00:18:08 -0500

    Added catcher on client side.

---
+++ labs/lab2/mazewar/Catcher.java
@@ -0,0 +1,28 @@
+// import java.io.*;
+// import java.net.*;
+// import java.util.concurrent.ConcurrentHashMap;
+// import java.util.concurrent.LinkedBlockingQueue;
+
+// /* Catcher class
+//  * Continually poles for server messages and adds to sorted client array
+//  *
+//  */
+// public class Catcher extends Thread {
+//     Array clientEvents;
+
+//     // Continually check for any packet messages and add it to the client events.
+//     public void run(){
+// 	String new_event = null;
+// 	int seqNum = 0;
+
+// 	while(true){ //???
+// 	    MazePacket messageFromServer = null;
+// 	    in.(messageFromServer);
+
+// 	    clientEvents[messageFromServer.seqNum] = clientEvents.message;
+// 	}
+
+//     }
+
+// }
+
