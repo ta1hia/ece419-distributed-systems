26748bba5eaa91d6e4c7c0d957285f79c92299c6
Tahia Khan <tahia.khan@gmail.com>
2014-03-27 19:01:54 -0400

    Adding skeleton of listener thread fcn in jt

--- labs/lab4/unhasher/src/unhasher/TaskPacket.java
+++ labs/lab4/unhasher/src/unhasher/TaskPacket.java
@@ -4,6 +4,11 @@ import java.io.Serializable;
 
 @SuppressWarnings("serial")
 public class TaskPacket implements Serializable {
+	
+	// Packet Type
+	public static Integer TASK_SUBMIT =		100;
+	public static Integer TASK_QUERY =		101;
+
 
 	
 
